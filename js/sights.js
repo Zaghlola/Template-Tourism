03e0c55504f4840606fab7272520c7deb2a7c504
a-fayez <ahmedf.higgy@gmail.com>
2020-11-19 03:56:34 +0200

    add content from local storage

--- js/sights.js
+++ js/sights.js
@@ -1,6 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-    if(localStorage.getItem("sights")){
-        const sights = localStorage.getItem("sights")
-        
+    if (localStorage.getItem("sights")) {
+        const sightsObj = JSON.parse(localStorage.getItem("sights"))
+        sightsObj.sights.forEach(sight => {
+            // rink div
+            const rink = document.createElement("div")
+            rink.classList.add("rink")
+
+            const text = document.createElement("div")
+            text.classList.add("sample_text")
+
+            const header = document.createElement("h3")
+            header.innerHTML = sight.name
+
+            const description = document.createElement("p")
+            description.innerHTML = sight.description
+
+            text.appendChild(header)
+            text.appendChild(description)
+
+            rink.appendChild(text)
+
+            document.querySelector("div .con").appendChild(rink)
+        })
     }
 })
